b01324a8e22828b2c6e109840412c4d7648f7065
omadonex <omadonex@yandex.ru>
2023-01-14 17:52:46 +0300

    wip

---
+++ src/types/ContextTypeEnum.ts
@@ -0,0 +1,8 @@
+export enum ContextTypeEnum {
+    primary = 'primary',
+    secondary = 'secondary',
+    success = 'success',
+    danger = 'danger',
+    warning = 'warning',
+    info = 'info',
+}
