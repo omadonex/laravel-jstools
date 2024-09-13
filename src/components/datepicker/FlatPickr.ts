b5a14549662769c63a4074e2843e960f52cf1e9d
omadonex <omadonex@yandex.ru>
2024-09-13 20:26:25 +0300

    flatpickr

--- src/components/datepicker/FlatPickr.ts
+++ src/components/datepicker/FlatPickr.ts
@@ -5,13 +5,19 @@ import flatpickr from 'flatpickr';
 export class FlatPickr implements ComponentContract {
   private id: string;
   private flatpickr: any;
+  private options: AnyObjInterface;
 
   constructor(id: string, options: AnyObjInterface) {
     this.id = id;
     this.flatpickr = flatpickr(`#${id}`, options);
+    this.options = options;
   }
 
   public getValue(): null | string {
+    if (this.options.mode === undefined) {
+      return this.flatpickr.selectedDates[0] || null;
+    }
+    
     return this.flatpickr.selectedDates;
   }
 
