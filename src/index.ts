25776f23a09193e69a29b615645c7eee6bcb5674
omadonex <omadonex@yandex.ru>
2025-04-03 16:42:37 +0300

    fixes form validation

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ export const Test = (str: string) => `Test ${str}`;
 // ---Misc---
 import { JSToolsAbstractMap } from './app/JSToolsAbstractMap';
 import { App } from './app/App';
-export { JSToolsAbstractMap, App };
+import { Page } from "./services/PageService/Page";
+
+export { JSToolsAbstractMap, App, Page };
 
 // ---Contracts
 import { FormContract } from './entities/Form/contracts/FormContract';
@@ -11,6 +13,7 @@ import { ModalContract } from './entities/Modal/contracts/ModalContract';
 import { AxiosServiceContract } from './services/AxiosService/contracts/AxiosServiceContract';
 import { FormValidateServiceContract } from './services/FormValidateService/contracts/FormValidateServiceContract';
 import { NotyServiceContract } from './services/NotyService/contracts/NotyServiceContract';
+import { PageServiceContract } from "./services/PageService/contracts/PageServiceContract";
 import { TranslateServiceContract } from './services/TranslateService/contracts/TranslateServiceContract';
 import { ValidateServiceContract } from './services/ValidateService/contracts/ValidateServiceContract';
 export {
@@ -19,6 +22,7 @@ export {
   AxiosServiceContract,
   FormValidateServiceContract,
   NotyServiceContract,
+  PageServiceContract,
   TranslateServiceContract,
   ValidateServiceContract,
 };
